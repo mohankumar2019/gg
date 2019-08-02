0f40873e3db9309efafc21f80a2fe9046375ff1b
mohan <mohan.chanti4@gmail.com>
2019-08-02 14:02:53 +0530

    new changes

---
+++ src/main/java/org/com/app/mvngg/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mvngg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mvngg" );
+    }
+}
